f69664af419a858e0d7057578e43aef936fbb81e
lukasprogram <lukas.je@post.cz>
2026-09-21 00:04:26 +0200

    changes

---
+++ GuitLong/GuitLong/Code/Models/Section.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuitLong.Code.Models
+{
+    public class Section
+    {
+        public string Name { get; set; } = "";
+        public List<Row> Rows { get; set; } = new List<Row>();
+    }
+    public class Row 
+    { 
+        public List<Chord> Chords { get; set; } = new List<Chord>();
+        public string Lyrics { get; set; } = "";
+    }
+
+    public class Chord
+    {
+        public int CharacterPosition = 0;
+        public string Name { get; set; } = "";
+    }
+
+    public class StrummingPattern
+    {
+        public List<Strum> Strums { get; set; } = new();
+    }
+
+    public class Strum
+    {
+        public StrumDirection Direction { get; set; }
+        public double Beat { get; set; }
+    }
+
+    public enum StrumDirection
+    {
+        Down,
+        Up,
+        None
+    }
+
+
+}
--- GuitLong/GuitLong/Code/Models/Song.cs
+++ GuitLong/GuitLong/Code/Models/Song.cs
@@ -9,7 +9,7 @@ namespace GuitLong.Code.Models
         public string Title { get; set; } = "";
         public string Author { get; set; } = "";
 
-        public string Capo { get; set; } = "";
-        public string Content { get; set; } = "";
+        public int? Capo { get; set; }
+        public List<Section> Sections { get; set; } = new List<Section>();
     }
 }
